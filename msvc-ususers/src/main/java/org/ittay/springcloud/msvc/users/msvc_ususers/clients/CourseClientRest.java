bc2be8fedfedacf4b40692054a65560fe9e3b625
IttayBarr <ittaybarrientos@gmail.com>
2025-07-29 07:10:10 -0600

    feat: Microservices connection updated

--- msvc-ususers/src/main/java/org/ittay/springcloud/msvc/users/msvc_ususers/clients/CourseClientRest.java
+++ msvc-ususers/src/main/java/org/ittay/springcloud/msvc/users/msvc_ususers/clients/CourseClientRest.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-courses", url = "msvc-courses:8002")
+@FeignClient(name = "msvc-courses", url = "${msvc.courses.url}")
 public interface CourseClientRest {
 
     @DeleteMapping("/course-user/{id}")
